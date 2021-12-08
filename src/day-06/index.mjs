14e176741ec8e82f266009e4159e0ab5f1701168
Pablo Castañeda Valdés <pcastaneda@dreyfous.com>
2021-12-08 10:44:58 -0400

    Day 06 with a faster leaner approach

--- src/day-06/index.mjs
+++ src/day-06/index.mjs
@@ -1,75 +1,25 @@
 import {readLines, inc, dec, linesToNumbers} from '../helpers/streams.mjs'
 import Lazy from 'lazy.js'
 
-const readings = await readLines(import.meta.url, 'input.test')
-
-class Lanternfish {
-
-  constructor(timer) {
-    this.timer = timer
-  }
-
-  age() {
-    if (this.timer)
-      this.timer -= 1
-    else {
-      this.timer = 6
-      return new Lanternfish(8)
-    }
-  }
-
-  static create(timer) {
-    return new Lanternfish(timer)
-  }
-
-  static age (fishes, fish) {
-    const newFish = fish.age()
-    if (newFish) fishes.push(newFish)
-    return fishes
-  }
-
-  static toTimersString(school) {
-    return school.map(fish => fish.timer).join(',')
-  }
-
-}
-
-
-const initialSchool = await readings
+const school = await readLines(import.meta.url, 'input')
   .map(l => l.split(','))
   .flatten()
   .map(n => parseInt(n))
-
-let school = await initialSchool
-  .map(Lanternfish.create)
-  .toArray()
-
-//console.log(`Initial state: ${Lanternfish.toTimersString(school)}`)
-
-for (let day = 1; day <= 80; day++) {
-  school = school.concat(school.reduce(Lanternfish.age, []))
-  //console.log(`${school.length} fishes after ${day} day${day > 1 ? 's: ' : ':  '} ${Lanternfish.toTimersString(school)}`)
-}
-
-console.log('Part 1: ', school.length, ' fishes.')
-
-let secondSchool = await initialSchool
   .reduce((fishes, fish) => {
     fishes[fish] = (fishes[fish] + 1) || 1
     return fishes
   }, {})
 
-const schedule = Array(7).fill(0).map((n, i) => secondSchool[i] || n)
-const newbornSchedule = Array(9).fill(0)
-
-//console.log(`Initial: ${scheduleToString(schedule)},${scheduleToString(newbornSchedule)}`)
+const schedule = Array(7).fill(0).map((n, i) => school[i] || n)
+const maturing = Array(9).fill(0)
 
 for (let day = 0; day < 256; day++) {
   const id = day % 7
-  const newborns = newbornSchedule.shift() 
-  if (newborns) schedule[id] += newborns 
-  newbornSchedule.push(schedule[id])
+  const newadults = maturing.shift() 
+  if (newadults) schedule[id] += newadults
+  maturing.push(schedule[id])
+  if (day == 79)
+    console.log('Part 1: ', schedule.reduce(inc) + maturing.reduce(inc), ' fishes.')
 }
 
-
-console.log('Part 2: ', schedule.reduce(inc) + newbornSchedule.reduce(inc), ' fishes.')
+console.log('Part 2: ', schedule.reduce(inc) + maturing.reduce(inc), ' fishes.')
