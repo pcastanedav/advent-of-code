fbbec8011a9d53fc3d44ef3e0f51366b9ed0f134
Pablo Castañeda Valdés <pcastaneda@dreyfous.com>
2021-12-13 08:46:14 -0400

    Day 13 done

--- src/day-13/index.mjs
+++ src/day-13/index.mjs
@@ -3,7 +3,7 @@ import * as math from 'mathjs'
 import Lazy from 'lazy.js'
 
 
-const lines = await readLines(import.meta.url, 'input.test')
+const lines = await readLines(import.meta.url, 'input')
 
 const paper = await lines.takeWhile(s => /^\d/.test(s))
   .map(s => s.split(',').map(s => parseInt(s)))
@@ -26,26 +26,41 @@ const matrix = paper.reduce((acc, [x, y]) => {
 
 function foldMatrix(matrix, {axis, value}) {
   const [height, width] = matrix.size() 
-  const x1 = math.range(0, axis == 'x' ? value : width)
-  const x2 = math.range(axis == 'x' ? value + 1 : 0, width)
-  const y1 = math.range(0, axis == 'y' ? value : height)
-  const y2 = math.range(axis == 'y' ? value + 1 : 0, height)
-  const mirror = m => axis == 'y'
-    ? math.matrixFromRows(...matrix.rows().map(s => s.toArray().reverse()))
-    : math.matrixFromColumns(...matrix.columns().map(s => s.toArray().reverse()))
-  const b = matrix.subset(math.index(y2, x2))
-  return math.or(matrix.subset(math.index(y1, x1)), b)
+  const vertical = axis == 'x'
+  const x1 = math.range(0, vertical ? value : width)
+  const x2 = math.range(vertical ? value + 1 : 0, width)
+  const y1 = math.range(0, vertical ? height : value)
+  const y2 = math.range(vertical ? 0 : value + 1, height)
+  const A = matrix.subset(math.index(y1, x1))
+  const B = matrix.subset(math.index(y2, x2))
+  const mirror = M => {
+    const [height, width] = M.size()
+    return M.map((a, [y0, x0]) => {
+      const y1 = vertical ? y0 : (height - 1) - y0
+      const x1 = vertical ? (width - 1) - x0 : x0
+      return M.subset(math.index(y1, x1))
+  })}
+  const MB = mirror(B)
+  return math.or(A, MB)
 }
+
 function matrixToString(matrix) {
-  return  matrix.map(s => s ? '#' : '.').toArray().map(s => s.join(''))
+  return  matrix.map(s => s ? '▌' : ' ').toArray().map(s => s.join(''))
 }
 
-const transformed = foldingInstructions.reduce((matrix, instruction) => {
-    return foldMatrix(matrix, instruction)
-}, matrix)
+function foldMatrixUsingInstructions(M, ops) {
+  return ops.reduce(foldMatrix, M)
+}
 
-console.table(matrixToString(foldMatrix(matrix, {axis: 'y', value: 7})))
-console.table(matrixToString(matrix))
+function countDots(M) {
+  return math.flatten(M).toArray().reduce(inc, 0)
+}
+
+const transformOnce = foldMatrix(matrix, foldingInstructions[0])
+
+
+console.log('Part 1: ', countDots(transformOnce))
+
+const transformed = foldMatrixUsingInstructions(matrix, foldingInstructions)
+
 console.table(matrixToString(transformed))
-
-console.log('Part 1: \n')
